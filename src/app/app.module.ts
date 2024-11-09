bee876fd93c4e39609a7d905a9f370a601baca71
dinnarus <99605736+Malek-Dinari@users.noreply.github.com>
2024-11-09 12:28:44 +0100

    Updated no standalone build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UpdateProviderComponent } from './update-provider/update-provider.component';
+import { ListProviderComponent } from './list-provider/list-provider.component';
+import { AddProviderComponent } from './add-provider/add-provider.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UpdateProviderComponent,
+    ListProviderComponent,
+    AddProviderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
